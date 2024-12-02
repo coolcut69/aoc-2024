beddca915db4dce875496371f65a3cd2e1cb0392
cborghmans <chris.borghmans@dpgmedia.be>
2024-12-02 07:50:07 +0100

    Day 1 part two

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,13 +1,13 @@
-import kotlin.math.max
+
+fun maxOfInts(leftInt: Int, rightInt: Int) : Int{
+    return if (leftInt > rightInt) {
+        leftInt - rightInt
+    } else {
+        rightInt - leftInt
+    }
+}
 
 fun main() {
-    fun maxOfInts(leftInt: Int, rightInt: Int) : Int{
-        if (leftInt > rightInt) {
-            return  leftInt - rightInt
-        } else {
-            return  rightInt - leftInt
-        }
-    }
 
     fun part1(input: List<String>): Int {
         val leftList = ArrayList<Int>()
@@ -22,12 +22,24 @@ fun main() {
         for ((index, i) in leftList.withIndex()) {
             sum += maxOfInts(i, rightList[index])
         }
-//        sum += maxOfInts(leftList.max(), rightList.max())
         return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val leftList = ArrayList<Int>()
+        val rightList = ArrayList<Int>()
+        for (s in input) {
+            leftList.add(s.split("   ")[0].toInt())
+            rightList.add(s.split("   ")[1].toInt())
+        }
+        var sum: Int = 0
+        for (i in leftList) {
+            val count = rightList.count { it == i }
+            sum += i * count
+        }
+        return sum
+
+
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -36,6 +48,7 @@ fun main() {
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
