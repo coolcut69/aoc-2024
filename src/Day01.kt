ba02026bb0ea8c548b2bf1978b482584c9fdc909
cborghmans <chris.borghmans@dpgmedia.be>
2024-12-02 07:44:04 +0100

    Day 1 part one

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,29 @@
+import kotlin.math.max
+
 fun main() {
+    fun maxOfInts(leftInt: Int, rightInt: Int) : Int{
+        if (leftInt > rightInt) {
+            return  leftInt - rightInt
+        } else {
+            return  rightInt - leftInt
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val leftList = ArrayList<Int>()
+        val rightList = ArrayList<Int>()
+        for (s in input) {
+            leftList.add(s.split("   ")[0].toInt())
+            rightList.add(s.split("   ")[1].toInt())
+        }
+        leftList.sort()
+        rightList.sort()
+        var sum: Int = 0
+        for ((index, i) in leftList.withIndex()) {
+            sum += maxOfInts(i, rightList[index])
+        }
+//        sum += maxOfInts(leftList.max(), rightList.max())
+        return sum
     }
 
     fun part2(input: List<String>): Int {
@@ -8,11 +31,11 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+//    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
