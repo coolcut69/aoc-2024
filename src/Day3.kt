da734aba39dcac25338dd92101a359837f3ce3b1
cborghmans <chris.borghmans@dpgmedia.be>
2024-12-03 19:30:13 +0100

    Day 3 refactor to better regex's

--- src/Day3.kt
+++ src/Day3.kt
@@ -1,56 +1,41 @@
+private fun executeMuls(instructions: String): Int =
+    """mul\((\d{1,3}),(\d{1,3})\)"""
+        .toRegex()
+        .findAll(instructions)
+        .sumOf { match ->
+            match.groupValues
+                .drop(1)
+                .map { it.toInt() }
+                .reduce(Int::times)
+        }
+
+
+private fun executeDisabled(instructions: String): Int =
+    """(^|do\(\)).*?($|don't\(\))"""
+        .toRegex()
+        .findAll(instructions)
+        .sumOf { executeMuls(it.value) }
+
+
 fun main() {
 
-    fun parseMull(it: String): Long {
-        val substringAfter = it.substringBefore(")").substringAfter("mul(")
-        val split = substringAfter.split(",")
-        return split[0].toLong() * split[1].toLong()
+    fun part1(input: List<String>): Int {
+        val text = input[0]
+        return executeMuls(text)
     }
 
-    fun part1(input: List<String>): Long {
+    fun part2(input: List<String>): Int {
         val text = input[0]
-
-        val pattern = Regex("mul\\(\\d+,\\d+\\)")
-        val matches = pattern.findAll(text).map { it.value }.toList()
-        return matches.sumOf { parseMull(it) }
-    }
-
-    fun part2(input: List<String>): Long {
-        val text = input[0]
-        var sum: Long = 0
-        var toParse = text.substringBefore("don't()")
-        var leftOver = text.substringAfter("don't()")
-
-        val pattern = Regex("mul\\(\\d+,\\d+\\)")
-        var matches = pattern.findAll(toParse).map { it.value }.toList()
-        sum += matches.sumOf { parseMull(it) }
-
-        while (leftOver.contains(pattern)){
-            toParse = leftOver.substringAfter("do()")
-            if (toParse.contains("don't")){
-                leftOver = toParse.substringAfter("don't()")
-            } else {
-                toParse = leftOver.substringAfter("do()")
-                toParse = toParse.substringBefore("don't()")
-                matches = pattern.findAll(toParse).map { it.value }.toList()
-                sum += matches.sumOf { parseMull(it) }
-                break
-            }
-            toParse = toParse.substringBefore("don't()")
-
-            matches = pattern.findAll(toParse).map { it.value }.toList()
-            sum += matches.sumOf { parseMull(it) }
-
-        }
-        return sum
+        return executeDisabled(text)
     }
 
     val testInput = readInput("Day03_test")
 
-    check(part1(testInput) == 161L)
-    check(part2(testInput) == 48L)
+    check(part1(testInput) == 161)
+    check(part2(testInput) == 48)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day03")
-    check(part1(input) == 182619815L)
-    check(part2(input) == 80747545L)
+    check(part1(input) == 182619815)
+    check(part2(input) == 80747545)
 }
