25fd20d5cd273a431c475423338f5e452af23361
cborghmans <chris.borghmans@dpgmedia.be>
2024-12-03 13:38:00 +0100

    Day 3 part two

--- src/Day3.kt
+++ src/Day3.kt
@@ -14,19 +14,43 @@ fun main() {
         return matches.sumOf { parseMull(it) }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val text = input[0]
+        var sum: Long = 0
+        var toParse = text.substringBefore("don't()")
+        var leftOver = text.substringAfter("don't()")
+
+        val pattern = Regex("mul\\(\\d+,\\d+\\)")
+        var matches = pattern.findAll(toParse).map { it.value }.toList()
+        sum += matches.sumOf { parseMull(it) }
+
+        while (leftOver.contains(pattern)){
+            toParse = leftOver.substringAfter("do()")
+            if (toParse.contains("don't")){
+                leftOver = toParse.substringAfter("don't()")
+            } else {
+                toParse = leftOver.substringAfter("do()")
+                toParse = toParse.substringBefore("don't()")
+                matches = pattern.findAll(toParse).map { it.value }.toList()
+                sum += matches.sumOf { parseMull(it) }
+                break
+            }
+            toParse = toParse.substringBefore("don't()")
+
+            matches = pattern.findAll(toParse).map { it.value }.toList()
+            sum += matches.sumOf { parseMull(it) }
+
+        }
+        return sum
     }
 
     val testInput = readInput("Day03_test")
 
     check(part1(testInput) == 161L)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 48L)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day03")
-//    check(part1(testInput) == 1)
-//    check(part2(testInput) == 1)
-    part1(input).println()
-    part2(input).println()
+    check(part1(input) == 182619815L)
+    check(part2(input) == 80747545L)
 }
