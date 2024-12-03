de421464f5f0990d0f06634241c974d46181a11b
cborghmans <chris.borghmans@dpgmedia.be>
2024-12-03 08:07:10 +0100

    Day 3 part one

--- src/Day3.kt
+++ src/Day3.kt
@@ -1,24 +1,32 @@
-
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parseMull(it: String): Long {
+        val substringAfter = it.substringBefore(")").substringAfter("mul(")
+        val split = substringAfter.split(",")
+        return split[0].toLong() * split[1].toLong()
+    }
+
+    fun part1(input: List<String>): Long {
+        val text = input[0]
+
+        val pattern = Regex("mul\\(\\d+,\\d+\\)")
+        val matches = pattern.findAll(text).map { it.value }.toList()
+        return matches.sumOf { parseMull(it) }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
-
     }
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 3)
+
+    check(part1(testInput) == 161L)
+//    check(part2(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day03")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 1)
+//    check(part1(testInput) == 1)
+//    check(part2(testInput) == 1)
     part1(input).println()
     part2(input).println()
 }
