21a526e5130341b796f1187a5075254cee0179f0
cborghmans <chris.borghmans@dpgmedia.be>
2024-12-03 07:11:27 +0100

    Day 3 setup

---
+++ src/Day3.kt
@@ -0,0 +1,24 @@
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+
+    }
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 1)
+    check(part2(testInput) == 3)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day03")
+    check(part1(testInput) == 1)
+    check(part2(testInput) == 1)
+    part1(input).println()
+    part2(input).println()
+}
