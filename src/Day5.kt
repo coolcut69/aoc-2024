b2d93012962d9a765a062e4030e5d9492a7b7616
cborghmans <chris.borghmans@dpgmedia.be>
2024-12-05 07:08:24 +0100

    Day 6 setup

---
+++ src/Day5.kt
@@ -0,0 +1,24 @@
+
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+       return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("Day05_test")
+
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day04")
+    check(part1(input) == 0)
+    check(part2(input) == 0)
+}
+
+
